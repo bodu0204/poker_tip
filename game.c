d964e9f13de2c2122ecefa510bedbf078627953b
呂明 <ryoakira@ryoakiranoMacBook-Pro.local>
2022-01-18 20:24:43 +0900

    2nd

--- game.c
+++ game.c
@@ -1,4 +1,6 @@
 #include "poker_tip.h"
+#define ENTRY 1
+#define BET 0
 #define DROP 1
 #define CALL 2
 
@@ -12,6 +14,13 @@ void	game(player	*p, int	pn)
 	char	*s[32];
 
 	d_clear();
+	i = 0;
+	while (i < pn)
+	{
+		(p + i)->wallet -= ENTRY;
+		(p + i)->table += ENTRY;
+		i++;
+	}
 	d_start();
 	win = det_geme(p, pn);
 	if (win)
@@ -50,15 +59,30 @@ void	game(player	*p, int	pn)
 
 int		det_geme(player	*p, int	pn)
 {
-	int	i;
-	int	drop;
+	int		i;
+	int		ii;
+	char	s[32];
+	int		drop;
 
 	while (1)
 	{
 		i = 0;
 		while (i < pn && drop != pn - 1)
 		{
-			/* それぞれに賭けさせる */
+			if ((p + i)->declare != DROP || (p + i)->wallet)
+			{
+				/* 未完 */
+				d_choice(p, i);
+				ii = read(0, s, 32);
+				s[ii - 1] = '\0';
+				if (ii == 1 || !strcmp(s, "c") || !strcmp(s, "C") || !strcmp(s, "call") || !strcmp(s, "Call") || !strcmp(s, "CALL") || !strcmp(s, "0"))
+				{
+					(p + i)->declare = CALL;
+
+				}
+				/* 未完\0 */
+			}
+			i++;
 		}
 		i = 0;
 		if (drop == pn - 1)
